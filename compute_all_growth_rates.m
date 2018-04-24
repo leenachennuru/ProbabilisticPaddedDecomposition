fe887dffb3d06f01956a9f1ba205692d2de01757
Leena Suresh <leenthegenieass@gmail.com>
2018-04-24 12:28:11 +0200

    Update compute_all_growth_rates.m

--- compute_all_growth_rates.m
+++ compute_all_growth_rates.m
@@ -13,3 +13,6 @@ function all_growth_rates = compute_all_growth_rates(D,diam,gamma1,gamma2)
         all_growth_rates(i) = compute_growth_rate_D(i,D,diam,gamma1,gamma2);
     end
 end
+% Implementation of concepts from 
+%Ittai Abraham, Yair Bartal, Ofer Neiman, Advances in metric embedding theory, ...
+%Advances in Mathematics, Volume 228, Issue 6, 2011, Pages 3026-3126,
